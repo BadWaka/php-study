c1c19666082e44c002f00c7ff417f7b91f455fe8
BadWaka <1456683844@qq.com>
2017-09-18 20:32:58 +0800

    php对象

--- demo1.php
+++ demo1.php
@@ -102,8 +102,35 @@
 //$x = true;
 //$y = false;
 
-$cars = array("Volvo", "BMW", "Toyota");
-var_dump($cars);
+//$cars = array("Volvo", "BMW", "Toyota");
+//var_dump($cars);
+
+class Car
+{
+    var $color;
+
+    function Car($color = "green")
+    {
+        $this->color = $color;
+    }
+
+    function what_color()
+    {
+        return $this->color;
+    }
+}
+
+function print_vars($obj)
+{
+    foreach (get_object_vars($obj) as $prop => $val) {
+        echo "\t$prop = $val\n";
+    }
+}
+
+$herbie = new Car("white");
+
+echo "\herbie:Properties\n";
+print_vars($herbie);
 
 ?>
 
