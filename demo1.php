59f654d67fd8a0208ac3e75d81b89fb0af80b556
wangkai37 <wangkai37@baidu.com>
2017-09-18 19:28:23 +0800

    局部和全局作用域

--- demo1.php
+++ demo1.php
@@ -5,7 +5,30 @@
 <h1>My first PHP page</h1>
 
 <?php
-echo "Hello World!";
+//echo "Hello World!";
+//$x = 5;
+//$y = 6;
+//$z = $x + $y;
+//echo $z;
+//$txt='Hello World!';
+//echo $txt;
+$x = 5;
+
+function myTest()
+{
+    $y = 10;
+    echo '<p>测试函数内变量</p>';
+    echo "变量 x 为: $x";
+    echo '<br>';
+    echo "变量 y 为: $y";
+}
+
+myTest();
+
+echo "<p>测试函数外变量:<p>";
+echo "变量 x 为: $x";
+echo "<br>";
+echo "变量 y 为: $y";
 ?>
 
 </body>
