414c1faca05048a3932e0707c192b255c4336dd4
wangkai37 <wangkai37@baidu.com>
2017-09-18 20:23:04 +0800

    php浮点型 php布尔型

--- demo1.php
+++ demo1.php
@@ -78,17 +78,29 @@
 //$x = 'Hello world!';
 //echo $x;
 
-$x = 5985;
-var_dump($x);
-echo "<br>";
-$x = -345;
-var_dump($x);
-echo "<br>";
-$x = 0x8C;
-var_dump($x);
-echo "<br>";
-$x = 047;
-var_dump($x);
+//$x = 5985;
+//var_dump($x);
+//echo "<br>";
+//$x = -345;
+//var_dump($x);
+//echo "<br>";
+//$x = 0x8C;
+//var_dump($x);
+//echo "<br>";
+//$x = 047;
+//var_dump($x);
+
+//$x = 10.365;
+//var_dump($x);
+//echo "<br>";
+//$x = 2.4e3;
+//var_dump($x);
+//echo "<br>";
+//$x = 8E-5;
+//var_dump($x);
+
+$x = true;
+$y = false;
 
 ?>
 
