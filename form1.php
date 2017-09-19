1170648e74ad6a711b38d5ce85fc1f3f33e47197
BadWaka <1456683844@qq.com>
2017-09-19 15:10:47 +0800

    PHP

--- form1.php
+++ form1.php
@@ -1,14 +1,14 @@
 <html>
 <body>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     Name：<input type="text" name="fname">
     <input type="submit">
 </form>
 
 <?php
 
-$name = $_REQUEST['fname'];
+$name = $_POST['fname'];
 echo $name;
 
 ?>
