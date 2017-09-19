cd8974b25d3bf3caa8304fb7ed66d8087871c79d
BadWaka <1456683844@qq.com>
2017-09-19 14:03:52 +0800

    PHP Switch 语句

--- demo1.php
+++ demo1.php
@@ -290,14 +290,30 @@
 //echo $e;
 //echo "\n";
 
-$t = date("H");
-echo $t;
-if ($t < "10") {
-    echo "Have a good morning!";
-} elseif ($t < "20") {
-    echo "Have a good day!";
-} else {
-    echo "Have a good night!";
+//$t = date("H");
+//echo $t;
+//if ($t < "10") {
+//    echo "Have a good morning!";
+//} elseif ($t < "20") {
+//    echo "Have a good day!";
+//} else {
+//    echo "Have a good night!";
+//}
+
+$favcolor = "red";
+switch ($favcolor) {
+    case "red":
+        echo "你喜欢的颜色是红色!";
+        break;
+    case "blue":
+        echo "你喜欢的颜色是蓝色!";
+        break;
+    case "green":
+        echo "你喜欢的颜色是绿色!";
+        break;
+    default:
+        echo "你喜欢的颜色不是 红, 蓝, 或绿色!";
+        break;
 }
 
 ?>
