a82bb15c2be5dca585c39bc6f54c051cb3f48ed4
BadWaka <1456683844@qq.com>
2017-09-19 15:20:35 +0800

    PHP 循环 - While 循环

--- demo1.php
+++ demo1.php
@@ -385,7 +385,16 @@
 
 //echo serialize($_SERVER);
 
+$i = 1;
+//while ($i < 5) {
+//    echo "The number is " . $i . "<br>";
+//    $i++;
+//}
 
+do {
+    $i++;
+    echo "The number is " . $i . "<br>";
+} while ($i < 5);
 
 ?>
 
