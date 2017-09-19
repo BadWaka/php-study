17d0cadedbc40b60088a65ab15483695f9c2995b
BadWaka <1456683844@qq.com>
2017-09-19 13:24:47 +0800

    PHP 赋值运算符

--- demo1.php
+++ demo1.php
@@ -187,8 +187,32 @@
 //
 //echo -$x;
 
-var_dump(intdiv(10, 3));
-echo intdiv(10, 3);
+//var_dump(intdiv(10, 3));
+//echo intdiv(10, 3);
+
+$x = 10;
+echo $x;
+echo '<br>';
+
+$y = 20;
+$y += 100;
+echo $y;
+
+$z = 50;
+$z -= 25;
+echo $z; // 输出25
+
+$i = 5;
+$i *= 6;
+echo $i; // 输出30
+
+$j = 10;
+$j /= 5;
+echo $j; // 输出2
+
+$k = 15;
+$k %= 4;
+echo $k; // 输出3
 
 ?>
 
