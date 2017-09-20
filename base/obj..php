4e91d94a7e547f71dcf92dc5c6c559556de49419
wangkai37 <wangkai37@baidu.com>
2017-09-20 20:41:55 +0800

    构造函数

--- base/obj..php
+++ base/obj..php
@@ -11,6 +11,12 @@ class Site
     var $url;
     var $title;
 
+    function __construct($par1, $par2)
+    {
+        $this->url = $par1;
+        $this->title = $par2;
+    }
+
     function setUrl($par)
     {
         $this->url = $par;
@@ -36,14 +42,18 @@ $runoob = new Site();
 $taobao = new Site;
 $google = new Site;
 
-// 调用成员函数，设置标题和URL
-$runoob->setTitle("菜鸟教程");
-$taobao->setTitle("淘宝");
-$google->setTitle("Google 搜索");
+//// 调用成员函数，设置标题和URL
+//$runoob->setTitle("菜鸟教程");
+//$taobao->setTitle("淘宝");
+//$google->setTitle("Google 搜索");
+//
+//$runoob->setUrl('www.runoob.com');
+//$taobao->setUrl('www.taobao.com');
+//$google->setUrl('www.google.com');
 
-$runoob->setUrl('www.runoob.com');
-$taobao->setUrl('www.taobao.com');
-$google->setUrl('www.google.com');
+$runoob = new Site('www.runoob.com', '菜鸟教程');
+$taobao = new Site('www.taobao.com', '淘宝');
+$google = new Site('www.google.com', 'Google 搜索');
 
 // 调用成员函数，获取标题和URL
 $runoob->getTitle();
