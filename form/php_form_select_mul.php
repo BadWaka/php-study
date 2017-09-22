646397914891d646ec9290a5a914ba54fa90f190
wangkai37 <wangkai37@baidu.com>
2017-09-22 20:34:45 +0800

    PHP 下拉菜单多选

--- form/php_form_select_mul.php
+++ form/php_form_select_mul.php
@@ -10,5 +10,17 @@ if (is_array($q)) {
         // PHP_EOL 为常量，用于换行
         echo $sites[$val] . PHP_EOL;
     }
+} else {
+    ?>
+    <form action="" method="post">
+        <select name="q[]" id="" multiple="multiple">
+            <option value="">选择一个站点：</option>
+            <option value="RUNOOB">Runoob</option>
+            <option value="GOOGLE">Google</option>
+            <option value="TAOBAO">Taobao</option>
+        </select>
+        <input type="submit" value="提交">
+    </form>
+    <?php
 }
 ?>
